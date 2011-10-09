ee03ef5530b650718383c14e75e978f3623456d2
Jason Coene <jcoene@gmail.com>
2011-10-08 22:20:04 -0400

    Remove _site from git

--- _site/assets/site.js
+++
@@ -1,9 +0,0 @@
-jQuery(function($){
-  $("section.tweets div.stream").tweet({
-    username: "jcoene",
-    avatar_size: 32,
-    count: 6,
-    loading_text: "loading...",
-    template: "{avatar}{text}{time}"
-  });
-});
